dc6c0096fadda6bd55b783855f4d37878f702f3d
ben-snyder <benjamin.snyder@cornerstone.edu>
2022-03-29 15:42:42 -0400

    commit on eventview

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,10 +7,13 @@ import { CalendarComponent } from './calendar/calendar.component';
 
 import { NewEventComponent } from './new-event/new-event.component';
 
+import { EventViewComponent } from './event-view/event-view.component';
+
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'calendar', component: CalendarComponent },
   { path: 'newevent', component: NewEventComponent },
+  { path: 'event-view', component: EventViewComponent }
 ];
 
 @NgModule({
